cc65b2ba2af55cf52af9722516f69f48f84e8f93
Gean_Vitor <103613296+GeanVitor@users.noreply.github.com>
2024-08-11 14:25:08 -0300

    Adicionar arquivos de projeto.

---
+++ ChatSignalR/Startup.cs
@@ -0,0 +1,28 @@
+using ChatSignalR.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatSignalR
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {            
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDefaultFiles(); 
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
